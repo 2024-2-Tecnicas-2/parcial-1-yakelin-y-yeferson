297e568604d40762e059aaea46deaa28242a276d
YefersonL <linaresjefferson1234@gmail.com>
2024-09-16 20:12:37 -0500

    Creada clase padre

--- src/main/java/com/mycompany/parcial1/Producto.java
+++ src/main/java/com/mycompany/parcial1/Producto.java
@@ -1,5 +1,23 @@
 package com.mycompany.parcial1;
 
 public class Producto {
+    String nombre;
+    String descripcion;
+    double precio;
+    int cantidad;
+
+    public Producto(String nombre, String descripcion, double precio, int cantidad) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio + ", cantidad=" + cantidad + '}';
+    }
+    
+    
     
 }
